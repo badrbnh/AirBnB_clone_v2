13590c3df7aed9f42f4d49b41daae9c42f879e1a
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-09-20 09:14:41 +0000

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""'Amenity' class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """represents an Amenity for a MySQL database
+    inherits from the SQLAlchemy Base and links to the MySQL table amenities
+    """
+    __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
+    else:
+        name = ""
