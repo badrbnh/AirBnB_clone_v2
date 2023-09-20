acabad4af7696fd4e26fb22fbd6f6d4559f60c34
badrbnh <badrechahide@gmail.com>
2023-09-20 03:17:17 +0100

    fixing Some errors in the updated classes

--- models/base_model.py
+++ models/base_model.py
@@ -23,12 +23,19 @@ class BaseModel:
                                                      nullable=False,
                                                      default=datetime.now())
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key != '__class__':
+                    self.__dict__[key] = value
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+            
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/state.py
+++ models/state.py
@@ -7,3 +7,15 @@ class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Base.sqlalchemy.Column(Base.sqlalchemy.String(128), nullable=False)
+    cities = Base.sqlalchemy.relationship("City", cascade="all, delete",
+                                          backref="state")
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of City instances"""
+        from models import storage
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
