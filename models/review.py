1a7d6d04bd4bcdd88f97e9ffef6b628c246313c6
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-09-20 09:16:03 +0000

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""'Review' class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """represents a review class for a MySQL database
+    inherits from the SQLAlchemy 'Base' and links to the MySQL table reviews
+    """
+    __tablename__ = "reviews"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(Text(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
