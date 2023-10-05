2af379454c2ffb9ae608692d9408776936b7c4b9
badrbnh <badrechahide@gmail.com>
2023-10-06 00:16:45 +0100

    fab fixe

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import *
 from os.path import exists, splitext
 
 
-env.hosts = ['ubuntu@54.87.180.223', 'ubuntu@52.23.245.155']
+env.hosts = ['54.87.180.223', '52.23.245.155']
 
 
 @task
@@ -20,11 +20,11 @@ def do_deploy(archive_path):
     tmp_path = "/tmp/{}".format(arch_name)
     data_path = "/data/web_static/releases/{}".format(file_name)
     # Fabric commands
-    put(arch_name, '/tmp/')
+    put(archive_path, '/tmp/')
     run('mkdir -p {}'.format(data_path))
-    run('tar tar -xzf {}  -C {}'.format(temp_path, data_path))
-    run('rm {}'.format(temp_path))
-    run('mv {}/web_static/* {}'.format(data_path, data_path))
-    run('rm -rf {}/web_static'.fomrat(data_path))
+    run('tar -xzf {}  -C {}'.format(tmp_path, data_path))
+    run('rm {}'.format(tmp_path))
+    run('mv {}/web_static/* {}/'.format(data_path, data_path))
+    run('rm -rf {}/web_static'.format(data_path))
     run('rm -rf /data/web_static/current')
     run('ln -s {} /data/web_static/current'.format(data_path))
