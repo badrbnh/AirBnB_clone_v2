4f4acf4415172431336c895cddf01f353dd5b517
badrbnh <badrechahide@gmail.com>
2023-10-05 23:14:33 +0100

    task 0

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python3
-""" Fabric script to distribute an archive to web servers """
+"""Script to deploy web static"""
+from fabric.api import *
+from os.path import exists, splitext
 
-from fabric.api import env, put, run
-from os.path import exists
+
+env.hosts = ['ubuntu@54.87.180.223', 'ubuntu@52.23.245.155']
 
 
-env.use_ssh_config = True
-env.user = 'ubuntu'
-env.hosts = ['54.87.180.223', '52.23.245.155']  # Replace with your server IPs
-
+@task
 def do_deploy(archive_path):
-    """ Distribute an archive to web servers """
+    """ Function that deply archive"""
+
+    # Checking if path exists
     if not exists(archive_path):
         return False
-
-    try:
-            archive_name = archive_path.split("/")[-1]
-            no_extension = archive_name.split(".")[0]
-            remote_folder = "/data/web_static/releases/{}".format(no_extension)
-
-            # Upload the archive to the /tmp/ directory on the server
-            put(archive_path, "/tmp/{}".format(archive_name))
-
-            # Uncompress the archive to the specified folder
-            run("mkdir -p {}".format(remote_folder))
-            run("tar -xzf /tmp/{} -C {}".format(archive_name, remote_folder))
-
-            # Delete the uploaded archive from the server
-            run("rm /tmp/{}".format(archive_name))
-
-            # Move contents to the appropriate location
-            run("mv {}/web_static/* {}".format(remote_folder, remote_folder))
-
-            # Remove the old symbolic link
-            run("rm -rf /data/web_static/current")
-
-            # Create a new symbolic link
-            run("ln -s {} /data/web_static/current".format(remote_folder))
-
-            return True
-
-    except Exception as e:
-            return False
+    # Spliting the files
+    arch_name = archive_path.split('/')[1]
+    file_name = splitext(arch_name)[0]
+    tmp_path = "/tmp/{}".format(arch_name)
+    data_path = "/data/web_static/releases/{}".format(file_name)
+    # Fabric commands
+    put(arch_name, '/tmp/')
+    run('mkdir -p {}'.format(data_path))
+    run('tar tar -xzf {}  -C {}'.format(temp_path, data_path))
+    run('rm {}'.format(temp_path))
+    run('mv {}/web_static/* {}'.format(data_path, data_path))
+    run('rm -rf {}/web_static'.fomrat(data_path))
+    run('rm -rf /data/web_static/current')
+    run('ln -s {} /data/web_static/current'.format(data_path))
