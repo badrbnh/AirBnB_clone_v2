e7cd3d496f86431b86bcc6dc3fe5a72ab54b3c89
badrbnh <badrechahide@gmail.com>
2023-10-20 23:04:34 +0100

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+if __name__ == '__main':
+    app.run(host='0.0.0.0', port=5000)
