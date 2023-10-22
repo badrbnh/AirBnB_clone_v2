2b2831c7090feafe47ecb41363cfe346e8b33513
badrbnh <badrechahide@gmail.com>
2023-10-22 16:30:27 +0100

    task  7 1

--- models/state.py
+++ models/state.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""'State' class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+
+storage_data = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """represents a state for a MySQL database
-    inherits from the SQLAlchemy Base and links to the MySQL table states
-    """
+    """State class"""
+
     __tablename__ = "states"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False, unique=True)
+    if storage_data == "db":
+        name = Column(String(128), nullable=False)
         cities = relationship('City', cascade="all,delete", backref="state")
     else:
         name = ""
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+
         @property
         def cities(self):
-            """getter attribute in case of file storage"""
+            """getter attribute"""
             from models import storage
-            from models.city import City
-            city_list = []
-            for city in storage.all(City).values():
+
+            list_cities = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    list_cities.append(city)
+            return list_cities
