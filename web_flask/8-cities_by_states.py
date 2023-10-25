b21f82287b27c89ae3e5979a5a2c8f9f09426c97
badrbnh <badrechahide@gmail.com>
2023-10-25 17:52:37 +0100

    test hazmir

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,21 +8,10 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    cities = storage.all(City).values()
-    sorted_cities = sorted(cities, key=lambda city: city.name)
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html',
-                           cities=sorted_cities, states=sorted_states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
